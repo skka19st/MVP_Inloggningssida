529d42f28631dfc06baee7e39e6e8ef37651d89f
Karin Skans <skka19st@student.ju.se>
2019-09-08 19:07:14 +0200

    backup

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
 // Bygg upp webbsidorna
-
+console.log("första");
 ByggMenySida();
-//ByggInloggadSida();
+ByggInloggadSida();
+
+// event-triggers, måste ligga här för att "synas" överallt
 let LoggainBtnKlick = document.getElementById("btnLoggaIn");
-console.log("första klick-lyssnaren gjord");
-// event-triggers
+let LoggautBtnKlick = document.getElementById("btnLoggaUt");
+
 
 // händelsehantering: klickat logga in-knappen
 LoggainBtnKlick.addEventListener("click", function() 
 {
     console.log("klickat logga in");
+
+    // gör meny-sidan osynlig
+    //document.getElementById("divMenyId").remove();
+
     //ta bort meny-sidan från dokumentet(webbsidan)
-    document.getElementById("divMenyId").remove();
-    ByggInloggadSida();
+    //document.getElementById("divMenyId").remove();
+    //ByggInloggadSida();
 
 })
 
 // händelsehantering: klickat logga ut-knappen
-//LoggautBtnKlick.addEventListener("click", function() 
-// {
-//     console.log("klickat logga ut");
-//     //ta bort välkomst-sidan från dokumentet(webbsidan)
-//     document.getElementById("divInloggadId").remove();
-//     ByggMenySida();
+LoggautBtnKlick.addEventListener("click", function() 
+{
+    console.log("klickat logga ut");
+    // knappen blir synlig
+    document.getElementById("divInloggadId").setAttribute("hidden","true");
+    document.getElementById("divMenyId").setAttribute("hidden","false");
+    //ta bort välkomst-sidan från dokumentet(webbsidan)
+    //document.getElementById("divInloggadId").remove();
+    //ByggMenySida();
 
-// })
+})
 
 // funktion: bygger upp första div-taggen
-    // div
-    //   fieldset
-    //      legend
-    //      inmatningsruta
-    //      button
 function ByggMenySida()
 {
     // rubrik
@@ -46,15 +50,17 @@ function ByggMenySida()
     kodText = "<button id='btnLoggaIn'> Logga in </button>";
     document.getElementById("divMenyId").insertAdjacentHTML
                     ("beforeend", kodText)
+
+
     console.log("färdig med meny-sidan");
 }
 
 function ByggInloggadSida()
 {
-    console.log("denna borde inte komma ut");
-    // rubrik
-    document.getElementById("rubrikId").innerText = 
-                "Välkommen, du är inloggad";
+    console.log("inloggad på denna sida");
+
+    // document.getElementById("rubrikId").innerText = 
+    // "Välkommen, du är inloggad";
 
     // div-tagg
     var kodText = "<div id='divInloggadId'></div>";
@@ -64,9 +70,10 @@ function ByggInloggadSida()
     kodText = "<button id='btnLoggaUt'> Logga ut </button>";
     document.getElementById("divInloggadId").insertAdjacentHTML
                     ("beforeend", kodText)
+    
+    // knappen blir synlig
+    //document.getElementById("btnLoggaUt").setAttribute("hidden","false");
 
-    // event-trigger
-    //let LoggautBtnKlick = document.getElementById("btnLoggaUt");
 }
 
 function ByggFelSida()
