f2559657c2c8f9279311014f9e2fd8e260aab717
Karin Skans <skka19st@student.ju.se>
2019-09-09 08:03:40 +0200

    meny och inloggad-sida

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // Bygg upp webbsidorna
-console.log("första");
 ByggMenySida();
 ByggInloggadSida();
+
+// är personen redan inloggad?
+if (9===9)
+{    
+    VisaInloggad();
+}
+else
+{
+    VisaMeny();
+}
 
 // event-triggers, måste ligga här för att "synas" överallt
 let LoggainBtnKlick = document.getElementById("btnLoggaIn");
@@ -11,70 +20,95 @@ let LoggautBtnKlick = document.getElementById("btnLoggaUt");
 // händelsehantering: klickat logga in-knappen
 LoggainBtnKlick.addEventListener("click", function() 
 {
-    console.log("klickat logga in");
-
-    // gör meny-sidan osynlig
-    //document.getElementById("divMenyId").remove();
-
-    //ta bort meny-sidan från dokumentet(webbsidan)
-    //document.getElementById("divMenyId").remove();
-    //ByggInloggadSida();
-
+    VisaInloggad();
 })
 
-// händelsehantering: klickat logga ut-knappen
+// händelsehantering: klickat logga ut-knappen 
 LoggautBtnKlick.addEventListener("click", function() 
 {
-    console.log("klickat logga ut");
-    // knappen blir synlig
-    document.getElementById("divInloggadId").setAttribute("hidden","true");
-    document.getElementById("divMenyId").setAttribute("hidden","false");
-    //ta bort välkomst-sidan från dokumentet(webbsidan)
-    //document.getElementById("divInloggadId").remove();
-    //ByggMenySida();
-
+    VisaMeny();
 })
 
-// funktion: bygger upp första div-taggen
-function ByggMenySida()
+// funktion: visa meny-sidan
+function VisaMeny()
 {
+    // används för att förkorta kod-raderna: 
+    var kodText;
+    var knappen; 
+
     // rubrik
     document.getElementById("rubrikId").innerText = 
                 "logga in för att komma vidare";
 
+    // göm utloggnings-knappen
+    var knappen = document.getElementById("divLoggaUtId");
+    knappen.removeAttribute("display");
+    knappen.setAttribute("hidden","true"); 
+
+    // visa logga in-knappen (ta bort non-display)
+    knappen = document.getElementById("divMenyId");
+    knappen.removeAttribute("hidden");
+    knappen.setAttribute("display","true"); 
+}
+
+// funktion: visa sida för inloggad person
+function VisaInloggad()
+{
+    // används för att förkorta kod-raderna: 
+    var kodText;
+    var knappen; 
+
+    // rubrik
+    document.getElementById("rubrikId").innerText = 
+                "Välkommen, du är inloggad";
+
+    // göm logga in-knappen  
+    knappen = document.getElementById("divMenyId"); 
+    knappen.removeAttribute("display");
+    knappen.setAttribute("hidden","true"); 
+
+    // visa utloggnings-knappen (ta bort non-display)
+    knappen = document.getElementById("divLoggaUtId");
+    knappen.removeAttribute("hidden");
+    knappen.setAttribute("display","true"); 
+}
+
+// funktion: bygger upp första div-taggen
+function ByggMenySida()
+{
+    // används för att förkorta kod-raderna: 
+    var kodText;
+    var knappen; 
+
     // div-tagg
-    var kodText = "<div id='divMenyId'></div>";
+    kodText = "<div id='divMenyId'></div>";
     document.body.insertAdjacentHTML("beforeend",kodText);
 
     // inloggningsknappen
     kodText = "<button id='btnLoggaIn'> Logga in </button>";
-    document.getElementById("divMenyId").insertAdjacentHTML
-                    ("beforeend", kodText)
+    knappen = document.getElementById("divMenyId");
+    knappen.insertAdjacentHTML("beforeend", kodText)
 
-
-    console.log("färdig med meny-sidan");
+    knappen.setAttribute("display","true"); 
 }
 
 function ByggInloggadSida()
 {
-    console.log("inloggad på denna sida");
-
-    // document.getElementById("rubrikId").innerText = 
-    // "Välkommen, du är inloggad";
+    // används för att förkorta kod-raderna: 
+    var kodText;
+    var knappen; 
 
     // div-tagg
-    var kodText = "<div id='divInloggadId'></div>";
+    kodText = "<div id='divLoggaUtId'></div>";
     document.body.insertAdjacentHTML("beforeend",kodText);
 
     // inloggningsknappen
     kodText = "<button id='btnLoggaUt'> Logga ut </button>";
-    document.getElementById("divInloggadId").insertAdjacentHTML
-                    ("beforeend", kodText)
-    
-    // knappen blir synlig
-    //document.getElementById("btnLoggaUt").setAttribute("hidden","false");
+    knappen = document.getElementById("divLoggaUtId");
+    knappen.insertAdjacentHTML("beforeend", kodText);
 
-}
+    knappen.setAttribute("hidden","true"); 
+ }
 
 function ByggFelSida()
 {
